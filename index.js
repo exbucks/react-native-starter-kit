5569b44de71c6c19981ac1344b446516e668c5f4
Hiroki Moto <hiroki.moto.pro@gmail.com>
2018-06-23 13:00:05 +0900

    Fix register issue

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
 import App from './src/containers/app';
 
-AppRegistry.registerComponent('Reel', () => App);
+AppRegistry.registerComponent('CClub', () => App);
