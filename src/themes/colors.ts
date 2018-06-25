9cc45498f5de5c745b7f632bcf7ef2041617c6f5
TopDev130 <top.developer130@gmail.com>
2018-06-25 05:52:51 +0200

    add & update color variables

--- src/themes/colors.ts
+++ src/themes/colors.ts
@@ -1,13 +1,15 @@
 const colors = {
   primary: '#3969b0',
   black: '#000000',
-  grey: '#969faa',
+  grey: '#404040',
   darkgrey: '#696969',
   lightgrey: '#c3c7cc',
   lightergrey: '#eaedf0',
   transparent: 'rgba(0,0,0,0.0)',
   warmGrey: 'rgb(151,151,151)',
   white: 'rgb(255,255,255)',
+  purple: '#8A84D6',
+  shadowColor: '#00000020',
 }
 
 export default colors
