acddbf3664e955936186860a85bbd39fa94417e6
TopDev130 <top.developer130@gmail.com>
2018-07-02 05:44:35 +0200

    add iPhoneX detection to utils

--- src/services/utils.tsx
+++ src/services/utils.tsx
@@ -1,3 +1,17 @@
+import { Platform, Dimensions } from 'react-native'
+
 export function rand(min: number, max: number): number {
   return Math.floor(Math.random() * (max - min)) + min
+}
+
+export function isIPhoneX() {
+  const dimen = Dimensions.get('window')
+  return (
+    Platform.OS === 'ios' &&
+    (dimen.height === 812 || dimen.width === 812)
+  )
+}
+
+export function isAndroid() {
+  return Platform.OS === 'android'
 }
