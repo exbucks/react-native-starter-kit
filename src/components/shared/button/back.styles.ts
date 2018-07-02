43d6d90cff47c74fff473877af3e2e4f4bc2acce
TopDev130 <top.developer130@gmail.com>
2018-07-02 05:44:04 +0200

    add back button component

---
+++ src/components/shared/button/back.styles.ts
@@ -0,0 +1,12 @@
+import { ViewStyle } from 'react-native'
+import { isIPhoneX } from '../../../services'
+
+export const ROOT: ViewStyle = {
+  position: 'absolute',
+  left: 5,
+  top: isIPhoneX() ? 40 : 15,
+}
+
+export const button: ViewStyle = {
+  padding: 15,
+}
