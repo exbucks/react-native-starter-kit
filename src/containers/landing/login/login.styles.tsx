b60bba1a67523f4d380fe4a5292eb1d4392f0359
TopDev130 <top.developer130@gmail.com>
2018-07-06 18:49:44 +0900

    update login screen

--- src/containers/landing/login/login.styles.tsx
+++ src/containers/landing/login/login.styles.tsx
@@ -1,7 +1,79 @@
-import { ViewStyle, TextStyle, ImageStyle } from 'react-native'
+import { ViewStyle, TextStyle, ImageStyle, Dimensions } from 'react-native'
+import { colors } from '../../../themes'
+const window = Dimensions.get('window')
 
 export const ROOT: ViewStyle = {
   flex: 1,
   alignItems: 'center',
   justifyContent: 'center',
+  backgroundColor: colors.purple,
+  paddingTop: 20,
+}
+
+export const logoText: TextStyle = {
+  backgroundColor: 'transparent',
+  position: 'absolute',
+  top: window.height / 2 - 230,
+  left: window.width / 2,
+  marginLeft: -80,
+  fontFamily: 'Pacifico',
+  fontSize: 90,
+  color: colors.white,
+  padding: 5,
+  textShadowRadius: 2,
+  textShadowColor: colors.shadowColor,
+  textShadowOffset: { width: 1, height: 1 },
+}
+
+export const introText: TextStyle = {
+  color: colors.white,
+  textAlign: 'center',
+  fontSize: 14,
+}
+
+export const scrollContainer: ViewStyle = {
+  height: 200,
+  flexDirection: 'column',
+  alignItems: 'center',
+}
+
+export const scrollViewStyle: ViewStyle = {
+  height: 150,
+  width: window.width,
+  paddingVertical: 10,
+  paddingHorizontal: 5,
+  overflow: 'hidden',
+}
+
+export const scrollContentContainer: ViewStyle = {
+  alignItems: 'center',
+  justifyContent: 'center',
+}
+
+export const profileButton: ViewStyle = {
+  borderRadius: 18,
+  backgroundColor: colors.white,
+  width: '65%',
+  height: 36,
+  marginLeft: 3,
+  marginBottom: 10,
+}
+
+export const profileContainer: ViewStyle = {
+  flex: 1,
+  flexDirection: 'row',
+  alignItems: 'center',
+  padding: 10,
+  flexWrap: 'wrap',
+}
+
+export const profileImage: ImageStyle = {
+  width: 28,
+  height: 28,
+  borderRadius: 15,
+}
+
+export const profileName: TextStyle = {
+  color: colors.black,
+  marginLeft: 5,
 }
--- src/containers/landing/login/login.tsx
+++ src/containers/landing/login/login.tsx
@@ -1,9 +1,11 @@
 import * as React from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Text, TouchableOpacity, View, ScrollView, Image, TextBase } from 'react-native'
 import { NavigationScreenProps } from 'react-navigation'
 import { connect } from 'react-redux'
+import * as qs from 'query-string'
 import AppActions from '../../../actions/app'
 import * as screenStyles from './login.styles'
+import { create } from 'apisauce'
 
 export interface LoginScreenProps extends NavigationScreenProps<{}> {
   status: boolean
@@ -11,25 +13,83 @@ export interface LoginScreenProps extends NavigationScreenProps<{}> {
 }
 
 export interface LoginScreenState {
-  isBusy: boolean
+  isBusy: boolean,
+  phoneNumber: string,
+  accounts: Array<any>,
 }
 
 class Login extends React.Component<LoginScreenProps, LoginScreenState> {
+  api: any
+  AUTH_KEY: string = 'ffaf4b736f342c3c3aace3d86fb72341'
+  BASE_URL: string = 'https://www.net-networking.com/mobile_api'
+
   constructor(props) {
     super(props)
-    this.state = { isBusy: false }
+    const phoneNumber = props.navigation.getParam('phoneNumber', '')
+    this.state = {
+      isBusy: false,
+      phoneNumber: phoneNumber,
+      accounts: [],
+    }
+    console.log('*****', phoneNumber)
+    this.api = create({
+      baseURL: this.BASE_URL,
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': 'application/json',
+      },
+    })
+    this.getProfiles()
   }
 
   toLogin = () => {
     this.props.navigation.navigate('pin')
   }
 
+  getProfiles = async() => {
+    const { phoneNumber } = this.state
+
+    const reqBody = qs.stringify({
+      phone: phoneNumber,
+      AUTH_KEY: this.AUTH_KEY,
+    })
+
+    const response = await this.api.post('show_users_with_phone', reqBody)
+    console.log('*********', response)
+    this.setState({ accounts: response.data })
+  }
+
   render() {
+    const { accounts } = this.state
     return (
       <View style={screenStyles.ROOT}>
-        <TouchableOpacity onPress={this.toLogin}>
-          <Text>LOGIN</Text>
-        </TouchableOpacity>
+        <Text style={screenStyles.logoText}>
+          reel
+        </Text>
+        <View style={screenStyles.scrollContainer}>
+          <Text style={screenStyles.introText}>
+            Select the profile you wish to access:
+          </Text>
+          <ScrollView style={screenStyles.scrollViewStyle} contentContainerStyle={screenStyles.scrollContentContainer}>
+            {accounts.map((account, index) => {
+              const profileImage = `https://d1e9n6uw5itnwz.cloudfront.net/user/${account.user_id}_photo.jpg`
+              return (
+                <TouchableOpacity
+                  style={screenStyles.profileButton}
+                  key={`profile${index}`}
+                  onPress={() => {
+                    console.log('*****', index, profileImage)
+                  }}
+                >
+                  <View style={screenStyles.profileContainer}>
+                    <Image source={{uri: profileImage}} style={screenStyles.profileImage}/>
+                    <Text style={screenStyles.profileName}>{account.user_handle}</Text>
+                  </View>
+                </TouchableOpacity>
+              )
+            })}
+          </ScrollView>
+        </View>
       </View>
     )
   }
