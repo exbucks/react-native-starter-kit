c5c483db5e98c5858d96bcdc03c47ff498301835
TopDev130 <top.developer130@gmail.com>
2018-07-19 09:45:49 +0200

    update background style

--- src/containers/main/home/home.styles.tsx
+++ src/containers/main/home/home.styles.tsx
@@ -1,7 +1,9 @@
 import { ViewStyle, TextStyle, ImageStyle } from 'react-native'
+import { colors } from '../../../themes'
 
 export const ROOT: ViewStyle = {
   flex: 1,
   alignItems: 'center',
   justifyContent: 'center',
+  backgroundColor: colors.white,
 }
