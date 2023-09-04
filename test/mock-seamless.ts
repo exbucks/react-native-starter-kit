ce3a9fafb311fc1336ae4fb5fad46bad9e77821e
Hiroki Moto <hiroki.moto.pro@gmail.com>
2023-09-04 15:00:51 -0700

    remove old tests

--- test/mock-seamless.ts
+++
@@ -1,6 +0,0 @@
-jest.mock('seamless-immutable', () => {
-  return {
-    // because of the way `react-native-fbsdk` is built, we need a default export.
-    default: () => jest.fn().mockReturnValue(Promise.resolve()),
-  }
-})
